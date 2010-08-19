87eae011eed6b2eefacb0dc43c400c7c70f274f2
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2010-08-19 05:52:38 +0000

    Added a DatFileException class

---
+++ FshDatIO/DatFileException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FshDatIO
+{
+    class DatFileException : Exception
+    {
+        public DatFileException() : base("A DatFileException has occured")
+        {
+
+        }
+        public DatFileException(string message) : base(message)
+        { 
+        
+        }
+    }
+}

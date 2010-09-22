f7246e5eb29550862bb98b9b7cba51c70d43417c
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2010-09-22 03:15:33 +0000

    Initial commit

---
+++ FshDatIO/DatHeaderException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace FshDatIO
+{
+    public class DatHeaderException : DatFileException, ISerializable
+    {
+        public DatHeaderException() : base("A DatHeaderException has occured")
+        {
+        }
+
+        public DatHeaderException(string message) : base(message)
+        {
+        }
+        public DatHeaderException(string message, Exception inner) : base(message, inner)
+        {
+        }
+
+        protected DatHeaderException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}

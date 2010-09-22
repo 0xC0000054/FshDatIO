7928c28a300e2829ce3702b69c7fd510e8fe8f61
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2010-09-22 03:15:46 +0000

    Initial commit

---
+++ FshDatIO/DatIndexException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace FshDatIO
+{
+    public class DatIndexException : DatFileException, ISerializable  
+    {
+        public DatIndexException() : base("A DatIndexException has occured")
+        {}
+
+        public DatIndexException(string message) : base(message)
+        {
+        }
+        public DatIndexException(string message, Exception inner) : base(message, inner)
+        {
+        }
+
+        protected DatIndexException(SerializationInfo info, StreamingContext context) : base(info, context)
+        { }
+    }
+}

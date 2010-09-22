05aa735d1f29ab2834226abf1d582a3a3cf91f77
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2010-09-22 03:16:48 +0000

    Updated to comply with the MSDN Exception documentation

--- FshDatIO/DatFileException.cs
+++ FshDatIO/DatFileException.cs
@@ -1,18 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Runtime.Serialization;
 
 namespace FshDatIO
 {
-    class DatFileException : Exception
+    public class DatFileException : Exception, ISerializable
     {
         public DatFileException() : base("A DatFileException has occured")
         {
-
         }
+
         public DatFileException(string message) : base(message)
         { 
-        
         }
+
+        public DatFileException(string message, Exception inner) : base(message, inner)
+        {
+        }
+
+        protected DatFileException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+
     }
 }

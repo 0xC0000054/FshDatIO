4f7cb8e09cbd64aa8c5c528d641ca1126ea2463d
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2011-11-08 05:00:43 +0000

    Added a ProperRead extension method.

--- FshDatIO/StreamExtensions.cs
+++ FshDatIO/StreamExtensions.cs
@@ -24,5 +24,23 @@ namespace FshDatIO
             return (byte)val;
         }
 
+        public static void ProperRead(this Stream s, byte[] buffer, int offset, int count)
+        {
+            s.Seek((long)offset, SeekOrigin.Begin);
+
+            int numBytesToRead = count;
+            int numBytesRead = 0;
+            while (numBytesToRead > 0)
+            {
+                // Read may return anything from 0 to numBytesToRead.
+                int n = s.Read(buffer, numBytesRead, numBytesToRead);
+                // The end of the file is reached.
+                if (n == 0)
+                    break;
+                numBytesRead += n;
+                numBytesToRead -= n;
+            }
+        }
+
     }
 }

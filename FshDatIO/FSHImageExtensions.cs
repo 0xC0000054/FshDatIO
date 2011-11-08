ba6159aa058560a8b027dcf3027306223cead486
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2011-11-08 05:04:08 +0000

    Initial Commit.

---
+++ FshDatIO/FSHImageExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FSHLib;
+using System.Reflection;
+
+namespace FshDatIO
+{
+    static class FSHImageExtensions
+    {
+        private static FieldInfo rawDataField;
+        private static FieldInfo directoryField;
+        public static void SetRawData(this FSHImage fsh, byte[] data)
+        {
+            if (rawDataField == null)
+	        {
+		        Type fshImageType = typeof(FSHImage);
+                rawDataField = fshImageType.GetField("rawData", BindingFlags.Instance | BindingFlags.NonPublic);
+	        }
+            rawDataField.SetValue(fsh, data);
+        }
+
+        public static void SetDirectories(this FSHImage fsh, FSHDirEntry[] dirs)
+        {
+            if (directoryField == null)
+            {
+                Type fshImageType = typeof(FSHImage);
+                directoryField = fshImageType.GetField("directory", BindingFlags.Instance | BindingFlags.NonPublic);
+            }
+            directoryField.SetValue(fsh, dirs);
+        }
+    }
+}

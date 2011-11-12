3b55d8a963560f7fac43448c87b0ecf15f5e96e4
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2011-11-12 08:23:01 +0000

    Addded the CompressImage methods.

--- FshDatIO/Squish.cs
+++ FshDatIO/Squish.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Runtime.InteropServices;
 using System.Drawing.Imaging;
 using System.Drawing;
+using System.Diagnostics.CodeAnalysis;
+using System.Security.Permissions;
 
 namespace FshDatIO
 {
@@ -36,13 +38,17 @@ namespace FshDatIO
 
         private static class Squish_32
         {
-            [DllImport("squish_Win32.dll")]
+            [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1060:MovePInvokesToNativeMethodsClass"), DllImport("Squish_Win32.dll")]
+            internal static extern unsafe void SquishCompressImage(byte* rgba, int width, int height, byte* blocks, int flags);
+            [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1060:MovePInvokesToNativeMethodsClass"), DllImport("squish_Win32.dll")]
             internal static extern unsafe void SquishDecompressImage(byte* rgba, int width, int height, byte* blocks, int flags);
         }
 
         private static class Squish_64
         {
-            [DllImport("squish_x64.dll")]
+            [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1060:MovePInvokesToNativeMethodsClass"), DllImport("squish_x64.dll")]
+            internal static extern unsafe void SquishCompressImage(byte* rgba, int width, int height, byte* blocks, int flags);
+            [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1060:MovePInvokesToNativeMethodsClass"), DllImport("squish_x64.dll")]
             internal static extern unsafe void SquishDecompressImage(byte* rgba, int width, int height, byte* blocks, int flags);
         }
         private static unsafe void CallDecompressImage(byte[] rgba, int width, int height, byte[] blocks, int flags)
@@ -71,6 +77,77 @@ namespace FshDatIO
             return pixelOutput;
         }
 
+        [SecurityPermission(SecurityAction.LinkDemand, Flags = SecurityPermissionFlag.UnmanagedCode)]
+        public static unsafe byte[] CompressImage(Bitmap image, int flags)
+        {
+            if (image == null)
+                throw new ArgumentNullException("image", "image is null.");
+
+            byte[] pixelData = new byte[image.Width * image.Height * 4];
+
+            BitmapData data = image.LockBits(new Rectangle(0, 0, image.Width, image.Height), ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
+
+            try
+            {
+                byte* scan0 = (byte*)data.Scan0.ToPointer();
+                int srcStride = data.Stride;
+                int dstStride = (image.Width * 4);
+                fixed (byte* ptr = pixelData)
+                {
+                    for (int y = 0; y < image.Height; y++)
+                    {
+                        byte* p = scan0 + (y * srcStride);
+                        byte* q = ptr + (y * dstStride);
+                        for (int x = 0; x < image.Width; x++)
+                        {
+                            q[0] = p[2];
+                            q[1] = p[1];
+                            q[2] = p[0];
+                            q[3] = p[3];
+
+                            p += 4;
+                            q += 4;
+                        }
+                    }
+                }
+            }
+            finally
+            {
+                image.UnlockBits(data);
+            }
+
+            // Compute size of compressed block area, and allocate 
+            int blockCount = ((image.Width + 3) / 4) * ((image.Height + 3) / 4);
+            int blockSize = ((flags & (int)SquishFlags.kDxt1) != 0) ? 8 : 16;
+
+            // Allocate room for compressed blocks
+            byte[] blockData = new byte[blockCount * blockSize];
+
+            // Invoke squish::CompressImage() with the required parameters
+            CompressImageWrapper(pixelData, image.Width, image.Height, blockData, flags);
+
+            // Return our block data to caller..
+            return blockData;
+        }
+
+        [SecurityPermission(SecurityAction.LinkDemand, Flags = SecurityPermissionFlag.UnmanagedCode)]
+        private static unsafe void CompressImageWrapper(byte[] rgba, int width, int height, byte[] blocks, int flags)
+        {
+            fixed (byte* RGBA = rgba)
+            {
+                fixed (byte* Blocks = blocks)
+                {
+                    if (Is64Bit())
+                    {
+                        Squish_64.SquishCompressImage(RGBA, width, height, Blocks, flags);
+                    }
+                    else
+                    {
+                        Squish_32.SquishCompressImage(RGBA, width, height, Blocks, flags);
+                    }
+                }
+            }
+        }
         
     }
 }

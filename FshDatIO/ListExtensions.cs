6109406a83f226332ed79cbb7d8c9d8b08333834
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2011-11-15 08:13:48 +0000

    Removed the DirectoryEntry extension as it uses it's own collection.

--- FshDatIO/ListExtensions.cs
+++ FshDatIO/ListExtensions.cs
@@ -6,26 +6,11 @@ namespace FshDatIO
 {
     static class ListExtensions
     {
-        public static int Find(this List<DirectoryEntry> dir, uint Type, uint Group, uint Instance)
-        {
-            for (int i = 0; i < dir.Count; i++)
-            {
-                DirectoryEntry item = dir[i];
-                if ((item.Type == Type && item.Group == Group && item.Instance == Instance))
-                {
-                    return i;
-                }
-            }
-
-            return -1;
-        }
-
-        public static int Find(this List<DatIndex> indexes, uint Type, uint Group, uint Instance)
+        public static int Find(this List<DatIndex> indexes, uint type, uint group, uint instance)
         {
             for (int i = 0; i < indexes.Count; i++)
             {
-                DatIndex item = indexes[i];
-                if ((item.Type == Type && item.Group == Group && item.Instance == Instance))
+                if (indexes[i].Equals(type, group, instance))
                 {
                     return i;
                 }
@@ -40,9 +25,8 @@ namespace FshDatIO
         /// <returns>The FshWrapper at the specified index or null</returns>
         public static FshWrapper FromDatIndex(this List<FshWrapper> files, int index)
         {
-            for (int i = 0; i < files.Count; i++)
+            foreach (FshWrapper file in files)
             {
-                FshWrapper file = files[i];
                 if (file.FileIndex == index)
                 {
                     return file;

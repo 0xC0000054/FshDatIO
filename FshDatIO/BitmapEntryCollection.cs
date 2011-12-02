9d65acbf0c2a37615c4151f84bf5bdf5171f489c
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2011-12-02 08:02:09 +0000

    Initial Commit

---
+++ FshDatIO/BitmapEntryCollection.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FshDatIO
+{
+    /// <summary>
+    /// The collection of BitmapEntry items within the DatFile
+    /// </summary>
+    public sealed class BitmapEntryCollection : Collection<BitmapEntry>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BitmapEntryCollection"/> class.
+        /// </summary>
+        public BitmapEntryCollection() : base()
+        {
+        }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BitmapEntryCollection"/> class.
+        /// </summary>
+        /// <param name="capacity">The capacity.</param>
+        public BitmapEntryCollection(int capacity) : base(new List<BitmapEntry>(capacity))
+        {
+        }
+    }
+}

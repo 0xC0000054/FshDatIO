8d81da523953597f241c1bd33e11906d93f796c5
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2012-01-11 07:38:58 +0000

    Removed the unused ReadByte2 function.

--- FshDatIO/StreamExtensions.cs
+++ FshDatIO/StreamExtensions.cs
@@ -7,23 +7,6 @@ namespace FshDatIO
 {
     static class StreamExtensions
     {
-        /// <summary>
-        /// Reads a byte from the Stream and advances the read position by one byte 
-        /// </summary>
-        /// <returns>The byte read or throws an EndOfStreamException if the stream end has been reached</returns>
-        /// <exception cref="System.IO.EndOfStreamException">The end of the stream is reached.</exception>
-        public static byte ReadByte2(this Stream s)
-        {
-            int val = s.ReadByte();
-
-            if (val == -1)
-            {
-                throw new EndOfStreamException();
-            }
-
-            return (byte)val;
-        }
-
         public static void ProperRead(this Stream s, byte[] buffer, int offset, int count)
         {
             s.Seek((long)offset, SeekOrigin.Begin);

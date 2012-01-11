20f7d15589a3b6dd2f04d67f237c9cdb46e3ddfe
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2012-01-11 09:41:54 +0000

    Updated copyright year.

--- FshDatIO/Properties/AssemblyInfo.cs
+++ FshDatIO/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("null45")]
 [assembly: AssemblyProduct("FshDatIO")]
-[assembly: AssemblyCopyright("Copyright © 2011 Nicholas Hayes")]
+[assembly: AssemblyCopyright("Copyright © 2012 Nicholas Hayes")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 

4b881c23f8aaeeb031150f5a46b1abde2c7306ed
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2012-05-10 05:13:40 +0000

    Added the FSHTool compression methods.

--- FshDatIO/DXTComp.cs
+++ FshDatIO/DXTComp.cs
@@ -94,6 +94,7 @@ namespace FshDatIO
             }
 
         }
+        #region Squish Decompression
 
         /// <summary>
         /// Unpacks 565 packed color values.
@@ -206,6 +207,276 @@ namespace FshDatIO
                 rgba[index + 3] = (byte)(lo | (lo << 4));
                 rgba[index + 7] = (byte)(hi | (hi >> 4));
             }
+        } 
+        #endregion
+
+        public static unsafe byte[] CompressFSHToolDXT1(byte* scan0, int width, int height)
+        {
+            byte[] comp = new byte[((width * height) / 2) + 2000];
+
+
+            if (((height & 3) <= 0) && ((height & 3) <= 0))
+            {
+                int stride = 4 * width;
+                while ((stride & 4) > 0)
+                {
+                    stride++;
+                }
+                ulong* dxtPixels = stackalloc ulong[16];
+                fixed (byte* ptr = comp)
+                {
+                    int row, col, ofs, row2;
+                    int w2 = width / 4;
+                    int h2 = height / 4;
+                    for (int y = 0; y < w2; y++)
+                    {
+                        row = 4 * y;
+                        for (int x = 0; x < h2; x++)
+                        {
+                            col = 16 * x;
+                            for (int i = 0; i < 4; i++)
+                            {
+                                row2 = 4 * i;
+                                byte* p = (scan0 + ((row + i) * stride)) + col;
+                                for (int j = 0; j < 4; j++)
+                                {
+                                    ofs = 4 * j;
+                                    dxtPixels[row2 + j] = (ulong)((p[ofs] + (256 * p[ofs + 1])) + (65536 * p[ofs + 2]));
+                                }
+                            }
+
+                            PackDXT(dxtPixels, (ptr + ((2 * y) * width)) + (8 * x));
+                        }
+                    }
+                }
+            }
+
+
+            return comp;
         }
+
+        public static unsafe byte[] CompressFSHToolDXT3(byte* scan0, int width, int height)
+        {
+            byte[] comp = new byte[(width * height) + 2000];
+
+            int h = height / 4;
+            int w = width / 4;
+            int row, col, ofs, row2;
+
+
+            int stride = 4 * width;
+            while ((stride & 4) > 0)
+            {
+                stride++;
+            }
+
+            fixed (byte* dst = comp)
+            {
+                ulong* dxtPixels = stackalloc ulong[16];
+
+                for (int y = 0; y < h; y++)
+                {
+                    row = 4 * y;
+                    for (int x = 0; x < w; x++)
+                    {
+                        col = 16 * x;
+                        for (int i = 0; i < 4; i++)
+                        {
+                            row2 = 4 * i;
+                            byte* p = (scan0 + ((row + i) * stride)) + col;
+
+                            for (int j = 0; j < 4; j++)
+                            {
+                                ofs = 4 * j;
+
+                                dxtPixels[row2 + j] = (p[ofs] + (256U * p[ofs + 1]) + (65536U * p[ofs + 2]));
+                            }
+                        }
+
+                        PackDXT(dxtPixels, dst + ((row * width) + col) + 8);
+                    }
+                }
+
+                for (int y = 0; y < h; y++)
+                {
+                    row = 4 * y;
+                    for (int x = 0; x < w; x++)
+                    {
+                        ofs = 16 * x;
+                        col = ofs + 3; // get the alpha offset
+                        row2 = row * width;
+                        for (int i = 0; i < 4; i++)
+                        {
+                            byte* p = (scan0 + ((row + i) * stride)) + col;
+                            byte* tgt = dst + (row2 + ofs) + 2 * i;
+
+                            tgt[0] = (byte)(((p[0] & 0xf0) >> 4) + (p[4] & 0xf0));
+                            tgt[1] = (byte)(((p[8] & 0xf0) >> 4) + (p[12] & 0xf0));
+                        }
+                    }
+                }
+            }
+
+
+            return comp;
+        }
+
+        #region FSHTool DXT code
+        private static unsafe void PackDXT(ulong* px, byte* dest)
+        {
+            ulong[] uniq = new ulong[0x10];
+
+            int i, j;
+            ulong col1;
+            ulong col2;
+            int nstep = 0;
+            int bestErr = 0;
+            ulong bestCol1 = 0L;
+            ulong bestCol2 = 0L;
+            int nColors = 0;
+
+            for (i = 0; i < 0x10; i++)
+            {
+                col1 = px[i] & 0xf8fcf8UL;
+                j = 0;
+                while (j < nColors)
+                {
+                    if (uniq[j] == col1)
+                    {
+                        break;
+                    }
+                    j++;
+                }
+                if (j == nColors)
+                {
+                    uniq[nColors++] = col1;
+                }
+            }
+            if (nColors == 1)
+            {
+                bestCol1 = uniq[0];
+                bestCol2 = uniq[0];
+                bestErr = 0x3e8;
+                nstep = 3;
+            }
+            else
+            {
+                bestErr = 0x40000000;
+                for (i = 0; i < (nColors - 1); i++)
+                {
+                    for (j = i + 1; j < nColors; j++)
+                    {
+                        ulong dst;
+                        int err = ScoreDXT(px, 2, uniq[i], uniq[j], &dst);
+                        if (err < bestErr)
+                        {
+                            bestCol1 = uniq[i];
+                            bestCol2 = uniq[j];
+                            nstep = 2;
+                            bestErr = err;
+                        }
+                        err = ScoreDXT(px, 3, uniq[i], uniq[j], &dst);
+                        if (err < bestErr)
+                        {
+                            bestCol1 = uniq[i];
+                            bestCol2 = uniq[j];
+                            nstep = 3;
+                            bestErr = err;
+                        }
+                    }
+                }
+            }
+            byte* c1 = (byte*)&col1;
+            byte* c2 = (byte*)&col2;
+            col1 = bestCol1;
+            col2 = bestCol2;
+            ushort* sPtr = (ushort*)dest;
+            sPtr[0] = (ushort)(((c1[0] >> 3) + ((c1[1] >> 2) << 5)) + ((c1[2] >> 3) << 11));
+            sPtr[1] = (ushort)(((c2[0] >> 3) + ((c2[1] >> 2) << 5)) + ((c2[2] >> 3) << 11));
+            if ((sPtr[0] > sPtr[1]) ^ (nstep == 3))
+            {
+                ushort temp = sPtr[0];
+                sPtr[0] = sPtr[1];
+                sPtr[1] = temp;
+                bestCol1 = col2;
+                bestCol2 = col1;
+            }
+
+            ScoreDXT(px, nstep, bestCol1, bestCol2, (ulong*)(dest + 4));
+        }
+
+        private static unsafe int ScoreDXT(ulong* px, int nstep, ulong col1, ulong col2, ulong* pack)
+        {
+            int[] vec = new int[3];
+            int[] vdir = new int[3];
+
+            byte* c1 = (byte*)&col1;
+            byte* c2 = (byte*)&col2;
+
+            vdir[0] = c2[0] - c1[0];
+            vdir[1] = c2[1] - c1[1];
+            vdir[2] = c2[2] - c1[2];
+
+            int v2 = ((vdir[0] * vdir[0]) + (vdir[1] * vdir[1])) + (vdir[2] * vdir[2]);
+
+            int score = 0;
+            pack[0] = 0L;
+            byte* ptr = (byte*)(px + 15);
+            int i = 15;
+            int choice;
+            while (i >= 0)
+            {
+
+                vec[0] = ptr[0] - c1[0];
+                vec[1] = ptr[1] - c1[1];
+                vec[2] = ptr[2] - c1[2];
+                int xa2 = ((vec[0] * vec[0]) + (vec[1] * vec[1])) + (vec[2] * vec[2]);
+                int xav = ((vec[0] * vdir[0]) + (vec[1] * vdir[1])) + (vec[2] * vdir[2]);
+                if (v2 > 0)
+                {
+                    choice = ((nstep * xav) + (v2 >> 1)) / v2;
+                }
+                else
+                {
+                    choice = 0;
+                }
+
+                if (choice < 0)
+                {
+                    choice = 0;
+                }
+                else if (choice > nstep)
+                {
+                    choice = nstep;
+                }
+
+                score += (xa2 - (((2 * choice) * xav) / nstep)) + (((choice * choice) * v2) / (nstep * nstep));
+                pack[0] = pack[0] << 2;
+
+                if (choice == nstep)
+                {
+                    pack[0] += 1UL;
+                }
+                else if (choice > 0)
+                {
+                    pack[0] += (ulong)(choice + 1L);
+                }
+
+                i--;
+                ptr = (byte*)(px + i);
+            }
+
+            return score;
+        }
+
+        private static int LengthSquared(int[] vec)
+        {
+            return LengthSquared(vec, vec);
+        }
+        private static int LengthSquared(int[] lhs, int[] rhs)
+        {
+            return ((lhs[0] * rhs[0]) + (lhs[1] * rhs[1]) + (lhs[2] * rhs[2]));
+        }
+        #endregion
     }
 }

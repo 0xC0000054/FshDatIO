9823640c8212461b1975bbabc8c7b37afc305d79
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2012-05-10 05:53:17 +0000

    Initial commit.

---
+++ FshDatIO/FshDirEntry.cs
@@ -0,0 +1,17 @@
+﻿namespace FshDatIO
+{
+    /// <summary>
+    /// The structure that represents the FSH directory header.
+    /// </summary>
+    public struct FSHDirEntry
+    {
+        /// <summary>
+        /// The name of the directory.
+        /// </summary>
+        public byte[] name;
+        /// <summary>
+        /// The offset to the <see cref="EntryHeader"/>.
+        /// </summary>
+        public int offset;
+    }
+}

60db6465d4555255524b674860ed0747e1d8cbe9
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2012-05-10 05:35:57 +0000

    Updated documentation.

---
+++ FshDatIO/FshHeader.cs
@@ -0,0 +1,25 @@
+﻿namespace FshDatIO
+{
+    /// <summary>
+    /// The structure that represents the FSH file header. 
+    /// </summary>
+    public struct FSHHeader
+    {
+        /// <summary>
+        /// The FSH file identifier - 'SHPI'
+        /// </summary>
+        public byte[] SHPI;
+        /// <summary>
+        /// The total size it the file in bytes including this header
+        /// </summary>
+        public int size;
+        /// <summary>
+        /// The number of images within the file (may include a global palette).
+        /// </summary>
+        public int imageCount;
+        /// <summary>
+        /// The image family identifier.
+        /// </summary>
+        public byte[] dirID;
+    }
+}

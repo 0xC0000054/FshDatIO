c9211e25fe78e151607fa1e0bdab83c51c2345a1
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2014-07-30 00:14:41 +0000

    Changed the struct to a sealed class and the fields to properties, added a Save method.

--- FshDatIO/FshDirEntry.cs
+++ FshDatIO/FshDirEntry.cs
@@ -1,17 +1,76 @@
-﻿namespace FshDatIO
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace FshDatIO
 {
-    /// <summary>
-    /// The structure that represents the FSH directory header.
-    /// </summary>
-    public struct FSHDirEntry
-    {
-        /// <summary>
-        /// The name of the directory.
-        /// </summary>
-        public byte[] name;
-        /// <summary>
-        /// The offset to the <see cref="EntryHeader"/>.
-        /// </summary>
-        public int offset;
-    }
+	/// <summary>
+	/// The structure that represents the FSH directory header.
+	/// </summary>
+	public sealed class FSHDirEntry
+	{
+		private byte[] name;
+		private int offset;
+
+		/// <summary>
+		/// The name of the directory.
+		/// </summary>
+		public byte[] Name
+		{
+			get
+			{
+				return name;
+			}
+		}
+		
+		/// <summary>
+		/// The offset to the <see cref="EntryHeader"/>.
+		/// </summary>
+		public int Offset
+		{
+			get
+			{
+				return offset;
+			}
+			internal set
+			{
+				this.offset = value;
+			}
+		}
+
+		internal FSHDirEntry(BinaryReader reader)
+		{
+			if (reader == null)
+			{
+				throw new ArgumentNullException("reader");
+			}
+
+			this.name = reader.ReadBytes(4);
+			this.offset = reader.ReadInt32();
+		}
+
+		internal FSHDirEntry(string name)
+		{
+			if (name == null)
+			{
+				throw new ArgumentNullException("name");
+			}
+
+			if (name.Length == 4)
+			{
+				this.name = Encoding.ASCII.GetBytes(name);
+			}
+			else
+			{
+				this.name = Encoding.ASCII.GetBytes("0000");
+			}
+			this.offset = 0;
+		}
+
+		internal void Save(Stream stream)
+		{
+			stream.Write(this.name, 0, 4);
+			stream.WriteInt32(this.offset);
+		}
+	}
 }

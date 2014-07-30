d63a570b4c08c490fcfe429dbd0086515dc82d5a
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2014-07-30 08:33:12 +0000

    Initial commit

---
+++ FshDatIO/FshAttachment.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FshDatIO
+{
+	public sealed class FshAttachment
+	{
+		private EntryHeader header;
+		private byte[] data;
+		private bool binaryData;
+
+		public EntryHeader Header
+		{
+			get
+			{
+				return header;
+			}
+		}
+
+		public byte[] GetData()
+		{
+			return data;
+		}
+
+		public bool BinaryData
+		{
+			get
+			{
+				return binaryData;
+			}
+		}
+
+		internal FshAttachment(EntryHeader header, byte[] data, bool binaryData)
+		{
+			this.header = header;
+			this.data = data;
+			this.binaryData = binaryData;
+		}
+	}
+}

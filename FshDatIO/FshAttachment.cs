59fcf8a1a3734a8a8e3fc1eaebc76793b2e7b1fa
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2014-08-01 08:57:14 +0000

    Added XML documentation.

--- FshDatIO/FshAttachment.cs
+++ FshDatIO/FshAttachment.cs
@@ -1,15 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
+﻿
 namespace FshDatIO
 {
+	/// <summary>
+	/// The class that encapsulates an attachment within a FSH file.
+	/// </summary>
 	public sealed class FshAttachment
 	{
 		private EntryHeader header;
 		private byte[] data;
 
+		/// <summary>
+		/// Gets the attachment header.
+		/// </summary>
+		/// <value>
+		/// The attachment header.
+		/// </value>
 		public EntryHeader Header
 		{
 			get
@@ -18,11 +23,20 @@ namespace FshDatIO
 			}
 		}
 
+		/// <summary>
+		/// Gets the attachment data.
+		/// </summary>
+		/// <returns>The attachment data.</returns>
 		public byte[] GetData()
 		{
 			return data;
 		}
-		
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FshAttachment"/> class.
+        /// </summary>
+        /// <param name="header">The header.</param>
+        /// <param name="data">The data.</param>
 		internal FshAttachment(EntryHeader header, byte[] data)
 		{
 			this.header = header;

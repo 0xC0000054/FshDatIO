cc9623ee64aae76742146c44bc8de32c2f6578de
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2015-01-29 04:50:06 +0000

    Override the ClearItems and RemoveItem methods to dispose of the BitmapEntry, implemented IDisposable.

--- FshDatIO/BitmapEntryCollection.cs
+++ FshDatIO/BitmapEntryCollection.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 
 namespace FshDatIO
@@ -6,12 +7,14 @@ namespace FshDatIO
     /// <summary>
     /// The collection of BitmapEntry items within the FSHImageWrapper
     /// </summary>
-    public sealed class BitmapEntryCollection : Collection<BitmapEntry>
+    public sealed class BitmapEntryCollection : Collection<BitmapEntry>, IDisposable
     {
+        private bool disposed;
+
         /// <summary>
         /// Initializes a new instance of the <see cref="BitmapEntryCollection"/> class.
         /// </summary>
-        internal BitmapEntryCollection() : base()
+        internal BitmapEntryCollection() : this(0)
         {
         }
         /// <summary>
@@ -20,6 +23,62 @@ namespace FshDatIO
         /// <param name="capacity">The capacity.</param>
         internal BitmapEntryCollection(int capacity) : base(new List<BitmapEntry>(capacity))
         {
+            this.disposed = false;
         }
+        
+        protected override void ClearItems()
+        {
+            IList<BitmapEntry> items = Items;
+            for (int i = 0; i < items.Count; i++)
+            {
+                if (items[i] != null)
+                {
+                    items[i].Dispose();
+                    items[i] = null;
+                }
+            }
+
+            base.ClearItems();
+        }
+
+        protected override void RemoveItem(int index)
+        {
+            BitmapEntry entry = Items[index];
+
+            if (entry != null)
+            {
+                entry.Dispose();
+            }
+            
+            base.RemoveItem(index);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                this.disposed = true;
+
+                if (disposing)
+                {
+                    IList<BitmapEntry> items = Items;
+                    for (int i = 0; i < items.Count; i++)
+                    {
+                        if (items[i] != null)
+                        {
+                            items[i].Dispose();
+                            items[i] = null;
+                        }
+                    }
+                }
+            }
+        }
+
     }
 }

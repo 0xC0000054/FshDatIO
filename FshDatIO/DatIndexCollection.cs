aa7fe440623162ebac40f662e308c27c2f001600
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2015-03-03 03:26:54 +0000

    Override the CleaItems and RemoveItem methods, changed the IndexLocationComparer.Compare method to use Object.ReferenceEquals.

--- FshDatIO/DatIndexCollection.cs
+++ FshDatIO/DatIndexCollection.cs
@@ -17,6 +17,37 @@ namespace FshDatIO
             this.disposed = false;
         }
 
+        protected override void ClearItems()
+        {
+            IList<DatIndex> indices = Items;
+
+            for (int i = 0; i < indices.Count; i++)
+            {
+                DatIndex index = indices[i];
+
+                if (index.FileItem != null)
+                {
+                    index.FileItem.Dispose();
+                    index.FileItem = null;
+                }
+            }
+            
+            base.ClearItems();
+        }
+
+        protected override void RemoveItem(int index)
+        {
+            DatIndex item = Items[index];
+
+            if (item.FileItem != null)
+            {
+                item.FileItem.Dispose();
+                item.FileItem = null;
+            }
+            
+            base.RemoveItem(index);
+        }
+
         public DatIndex Find(uint type, uint group, uint instance)
         {
             IList<DatIndex> indices = Items;
@@ -91,10 +122,11 @@ namespace FshDatIO
                 {
                     for (int i = 0; i < Count; i++)
                     {
-                        if (Items[i].FileItem != null)
+                        DatIndex index = Items[i];
+                        if (index.FileItem != null)
                         {
-                            Items[i].FileItem.Dispose();
-                            Items[i].FileItem = null;
+                            index.FileItem.Dispose();
+                            index.FileItem = null;
                         }
                     }
                 }
@@ -109,32 +141,29 @@ namespace FshDatIO
 
             public int Compare(DatIndex x, DatIndex y)
             {
-                if (x != null)
+                if (Object.ReferenceEquals(x, y))
                 {
-                    if (y != null)
-                    { 
-                        if (x.Location < y.Location)
-                        {
-                            return -1;
-                        }
-                        else if (x.Location > y.Location)
-                        {
-                            return 1;
-                        }
-                        else
-                        {
-                            // The file locations should never be equal.
-                            return 0;
-                        }
-                    }
-
-                    return 1;
+                    return 0;
                 }
-                if (y != null)
+                if (x == null)
                 {
                     return -1;
                 }
+                if (y == null)
+                {
+                    return 1;
+                }
 
+                if (x.Location < y.Location)
+                {
+                    return -1;
+                }
+                else if (x.Location > y.Location)
+                {
+                    return 1;
+                }
+                
+                // The file locations should never be equal.
                 return 0;
             }
         }

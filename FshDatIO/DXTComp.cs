0c49358b0fad0127d19145cf01825fdbf2f51fb5
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2015-03-08 12:36:03 +0000

    Added the license header to the Squish code.

--- FshDatIO/DXTComp.cs
+++ FshDatIO/DXTComp.cs
@@ -1,13 +1,36 @@
 ﻿using System;
 
 namespace FshDatIO
-{
-    /// <summary>
-    /// DXT Decompression code based off Simon Brown's Squish library 
-    /// http://code.google.com/p/libsquish/
-    /// </summary>
+{ 
     static class DXTComp
     {
+        #region Squish Decompression Code
+
+        /* -----------------------------------------------------------------------------
+
+        Copyright (c) 2006 Simon Brown                          si@sjbrown.co.uk
+
+        Permission is hereby granted, free of charge, to any person obtaining
+        a copy of this software and associated documentation files (the
+        "Software"), to deal in the Software without restriction, including
+        without limitation the rights to use, copy, modify, merge, publish,
+        distribute, sublicense, and/or sell copies of the Software, and to
+        permit persons to whom the Software is furnished to do so, subject to
+        the following conditions:
+
+        The above copyright notice and this permission notice shall be included
+        in all copies or substantial portions of the Software.
+
+        THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+        OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+        MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+        IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+        CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+        TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+        SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+       
+        -------------------------------------------------------------------------- */
+
         /// <summary>
         /// Unpacks the DXT image.
         /// </summary>
@@ -33,7 +56,7 @@ namespace FshDatIO
                     int bytesPerBlock = dxt1 ? 8 : 16;
                     byte* targetRGBA = stackalloc byte[4 * 16];
                     byte* pBlock = pBlocks;
-                    byte* sourcePixel; // define the pointers outside the loop to help performance
+                    byte* sourcePixel;
                     byte* targetPixel;
                     for (int y = 0; y < height; y += 4)
                     {
@@ -78,7 +101,6 @@ namespace FshDatIO
 
             return pixelData;
         }
-        #region Squish Decompression
 
         /// <summary>
         /// Decompresses the DXT compressed block.
@@ -218,6 +240,8 @@ namespace FshDatIO
             }
         }
         #endregion
+        
+        #region FSHTool Compression code
 
         public static unsafe byte[] CompressFSHToolDXT1(byte* scan0, int width, int height)
         {
@@ -332,7 +356,6 @@ namespace FshDatIO
             return comp;
         }
 
-        #region FSHTool DXT code
         private static unsafe void PackDXT(uint* px, byte* dest)
         {
             uint[] uniq = new uint[0x10];

f5b5e1c189815a6c5f04e8efc9a6b8b9e437b035
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2015-08-20 22:18:04 +0000

    Removed the FillBuffer method.

--- FshDatIO/StreamExtensions.cs
+++ FshDatIO/StreamExtensions.cs
@@ -4,47 +4,39 @@ namespace FshDatIO
 {
     static class StreamExtensions
     {
-        private const int DefaultBufferSize = 4;
-        private static byte[] buffer;
-
-        private static void FillBuffer(Stream s, int count)
-        {
-            if (buffer == null)
-            {
-                buffer = new byte[DefaultBufferSize];
-            }
-
-            if (count > buffer.Length)
-            {
-                throw new System.ArgumentOutOfRangeException("count", "count > buffer.Length");
-            }
-
-            int bytesRead = 0;
-            int n;
-
-            do
-            {
-                n = s.Read(buffer, bytesRead, count - bytesRead);
-                if (n == 0)
-                {
-                    throw new EndOfStreamException();
-                }
-                bytesRead += n;
-
-            } while (bytesRead < count);
-        }
-
         /// <summary>
         /// Reads a 4-byte unsigned integer from the stream in little endian byte order and advances the position of the stream by four bytes.
         /// </summary>
         /// <param name="s">The stream.</param>
-        /// <exception cref="System.IO.EndOfStreamException">The end of the stream is reached.</exception>
+        /// <exception cref="EndOfStreamException">The end of the stream is reached.</exception>
         /// <returns>A 4-byte unsigned integer read from the current stream.</returns>
         public static uint ReadUInt32(this Stream s)
         {
-            FillBuffer(s, 4);
+            int byte1 = s.ReadByte();
+            if (byte1 == -1)
+            {
+                throw new EndOfStreamException();
+            }
 
-            return (uint)(buffer[0] | (buffer[1] << 8) | (buffer[2] << 16) | (buffer[3] << 24));
+            int byte2 = s.ReadByte();
+            if (byte2 == -1)
+            {
+                throw new EndOfStreamException();
+            }
+
+            int byte3 = s.ReadByte();
+            if (byte3 == -1)
+            {
+                throw new EndOfStreamException();
+            }
+
+            int byte4 = s.ReadByte();
+            if (byte4 == -1)
+            {
+                throw new EndOfStreamException();
+            }
+
+            return (uint)(byte1 | (byte2 << 8) | (byte3 << 16) | (byte4 << 24));
         }
 
         public static byte[] ReadBytes(this Stream s, int count)

1f1daf9a7b4221e64ca786e3a6cffd2f6a600648
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2017-08-24 06:48:19 +0000

    Added DirectoryEntryCollection.cs

---
+++ FshDatIO/DirectoryEntryCollection.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace FshDatIO
+{
+    internal sealed class DirectoryEntryCollection : Collection<DirectoryEntry>
+    {
+        public DirectoryEntryCollection() : this(0)
+        {
+        }
+
+        public DirectoryEntryCollection(int count) : base(new List<DirectoryEntry>(count))
+        {
+        }
+
+        public DirectoryEntry Find(uint type, uint group, uint instance)
+        {
+            IList<DirectoryEntry> items = Items;
+
+            for (int i = 0; i < items.Count; i++)
+            {
+                DirectoryEntry entry = items[i];
+                if (entry.Type == type && entry.Group == group && entry.Instance == instance)
+                {
+                    return entry;
+                }
+            }
+
+            return null;
+        }
+    }
+}
